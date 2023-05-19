d9330fe461082b5a498672e5a4dbdfa5ba7306b5
Alicja Barańska <alicja.baranska23@gmail.com>
2023-05-19 14:08:20 +0200

    Get city wearther

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
+    //http://api.weatherapi.com/v1/current.json?key=02e42614dc824161b30113915231905&q=Wroclaw&aqi=no
+    final response = await Dio().get(
+        'http://api.weatherapi.com/v1/current.json?key=02e42614dc824161b30113915231905&q=Wroclaw&aqi=no');
     return const WeatherModel(city: 'Warsaw', temperature: -5.5);
   }
 }
