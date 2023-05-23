aae86ec4631baf872f2e012f46c74eab62cc98e1
Alicja Barańska <alicja.baranska23@gmail.com>
2023-05-23 12:53:10 +0200

    Implement Data Source

---
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class WeatherRemoteDataSource {
+  Future<Map<String, dynamic>?> getWeatherData({
+    required String city,
+  }) async {
+    //http://api.weatherapi.com/v1/current.json?key=02e42614dc824161b30113915231905&q=Wroclaw&aqi=no
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=02e42614dc824161b30113915231905&q=$city&aqi=no');
+    return response.data;
+  }
+}
